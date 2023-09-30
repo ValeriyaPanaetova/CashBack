183c1ba917c6a2eaaa55cabb9b2a37f6b4409190
Valeriya <Lerusya.Pan@yandex.ru>
2023-09-30 22:13:15 +0700

    All my changes

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,53 +1,24 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class CashbackHackServiceTest {
     CashbackHackService service = new CashbackHackService();
 
     @Test
-    public void shouldReturn200at800() {
-        int expected = 200;
-        int actual = service.remain(800);
-
-        assertEquals(actual, expected);
-    }
-
-    @Test
-
-    public void shouldReturn1at999() {
-        int expected = 1;
-        int actual = service.remain(999);
-
-        assertEquals(actual, expected);
-    }
-
-    @Test
-
-    public void shouldReturn999at1() {
-        int expected = 999;
-        int actual = service.remain(1);
-
-        assertEquals(actual, expected);
-    }
-
-    @Test
-
-    public void shouldReturn0at1000() {
-        int expected = 0;
+    public void shouldReturnNull() {
         int actual = service.remain(1000);
-
-        assertEquals(actual, expected);
+        int expected = 0;
+        Assert.assertEquals(expected,actual);
+        //TODO: You need to fix that         28.09.2020
     }
 
     @Test
-
-    public void shouldReturn999at1001() {
-        int expected = 999;
-        int actual = service.remain(1001);
-
-        assertEquals(actual, expected);
+    public void shouldReturn200() {
+        int actual = service.remain(800);
+        int expected = 200;
+        Assert.assertEquals(expected,actual);
     }
-
 }
 
